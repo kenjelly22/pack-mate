41ad761307dd7b7e85c10aa453bc3bf343c70cee
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-09 11:07:51 -0400

    Add validations for Item attributes

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:category) }
+
+    # it "is valid with valid attributes" do
+    #   expect(Item.new).to be_valid
+    # end
+
+    it "is not valid without a name" do
+      item = build(:item)
+      item.name = nil
+      expect(item).not_to be_valid
+    end
+
+    # it "is not valid without a category" do
+    # end
+  end
 end
