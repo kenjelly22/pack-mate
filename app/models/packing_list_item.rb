f5625506be2b91adaf34d9e94cb1152c40b7bfad
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-08 21:12:44 -0400

    Add status validation to PackingListItem

--- app/models/packing_list_item.rb
+++ app/models/packing_list_item.rb
@@ -3,4 +3,5 @@ class PackingListItem < ApplicationRecord
   belongs_to :item
 
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :status, inclusion: { in: [ "packed", "not_packed" ] }
 end
