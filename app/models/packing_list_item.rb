b1f035850904afbb35cd85b1ea51940bff9050f1
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-08 20:28:47 -0400

    Add validations to PackingListItem

--- app/models/packing_list_item.rb
+++ app/models/packing_list_item.rb
@@ -1,4 +1,6 @@
 class PackingListItem < ApplicationRecord
   belongs_to :packing_list
   belongs_to :item
+
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end
