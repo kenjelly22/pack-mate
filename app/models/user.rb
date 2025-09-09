0f9a32fc5755daf8937e22a6af4e792985230490
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-09 16:12:18 -0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :packing_list_items
-  has_many :packing_lists, through: :packing_list_items
+  has_many :packing_lists
+  has_many :packing_lists_items, through: :packing_lists
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    email { "email@email.com" }
+    password { "password" }
+    name { "Stove" }
   end
 end
