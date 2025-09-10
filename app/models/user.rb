87dadb7235a6ce3a66496d844a8d8cc47c6c33db
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-10 10:10:46 -0400

    Add item association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
   has_many :packing_lists
-  has_many :packing_lists_items, through: :packing_lists
+  has_many :packing_list_items, through: :packing_lists
 end
