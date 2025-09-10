365ecd1233418e64582ef521568eea3cc96aeb7e
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-10 13:04:01 -0400

    Add packing_list_items to packing_list shoow page

--- app/controllers/packing_lists_controller.rb
+++ app/controllers/packing_lists_controller.rb
@@ -6,6 +6,7 @@ class PackingListsController < ApplicationController
   end
 
   def show
-    @packing_list = @packing_lists.find(params[:id])
+    @packing_list = current_user.packing_lists.find_by(params[:id])
+    @packing_list_items = @packing_list.items
   end
 end
