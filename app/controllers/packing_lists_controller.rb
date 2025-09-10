a585afdc199215b22548745ad969adc2487dd8cb
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-10 11:10:40 -0400

    Specify current_user in packing_list controller

--- app/controllers/packing_lists_controller.rb
+++ app/controllers/packing_lists_controller.rb
@@ -2,10 +2,10 @@ class PackingListsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @packing_lists = PackingList.all
+    @packing_lists = current_user.packing_lists
   end
 
   def show
-    @packing_list = PackingList.find(params[:id])
+    @packing_list = @packing_lists.find(params[:id])
   end
 end
