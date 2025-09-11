4d28f1461cb6c7d1a6e5b02d6c40ec88f4af6536
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-11 18:43:43 -0400

    Add error handling to update status in packing list form

--- app/controllers/packing_lists_controller.rb
+++ app/controllers/packing_lists_controller.rb
@@ -29,16 +29,20 @@ class PackingListsController < ApplicationController
   def update
     @packing_list = current_user.packing_lists.find(params[:id])
 
-  if params[:packed_item_ids].present?
-    @packing_list.packing_list_items.each do |pli|
-      new_status = params[:packed_item_ids].include?(pli.id.to_s) ? "packed" : "not_packed"
-      pli.update(status: new_status)
+    if params[:packed_item_ids].present?
+      @packing_list.packing_list_items.each do |pli|
+        new_status = params[:packed_item_ids].include?(pli.id.to_s) ? "packed" : "not_packed"
+        pli.update(status: new_status)
+      end
+    else
+      @packing_list.packing_list_items.update_all(status: "not_packed")
     end
-  else
-    @packing_list.packing_list_items.update_all(status: "not_packed")
-  end
 
-  redirect_to packing_list_path(@packing_list), notice: "Packing list updated!"
+    if @packing_list.update(packing_list_params)
+      redirect_to @packing_list, notice: "Packing list updated"
+    else
+      render :show, status: :unprocessable_entity
+    end
   end
 
   private
