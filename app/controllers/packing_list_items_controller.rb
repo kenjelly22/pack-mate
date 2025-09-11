8ffa1ae9148479cff74af661332bc83d4ffb0c3f
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-11 11:19:52 -0400

    Save new items to packing list and display

--- app/controllers/packing_list_items_controller.rb
+++ app/controllers/packing_list_items_controller.rb
@@ -1,9 +1,24 @@
 class PackingListItemsController < ApplicationController
   def new
     @packing_list = PackingList.find(params[:packing_list_id])
-    @items = Item.where(user_id: [ nil, current_user.id ])
+    @items = Item.all_for_user(current_user)
   end
 
   def create
+    packing_list = PackingList.find(params[:packing_list_id])
+
+    if params[:name].present? && params[:category].present?
+      new_item = Item.find_or_create_by(name: params[:name].titleize.strip, category: params[:category])
+      packing_list.items << new_item unless packing_list.items.include?(new_item)
+    end
+
+    if params[:item_ids].present?
+      items = Item.where(id: params[:item_ids])
+      items.each do |item|
+        packing_list.items << item unless packing_list.items.include?(item)
+      end
+    end
+
+    redirect_to packing_list, notice: "Items added successfully."
   end
 end
