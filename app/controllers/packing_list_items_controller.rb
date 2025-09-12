6134e8a81ca8617627fb664781e3d9c76d9e23ae
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-11 20:36:00 -0400

    Update message for when add item fails

--- app/controllers/packing_list_items_controller.rb
+++ app/controllers/packing_list_items_controller.rb
@@ -7,8 +7,11 @@ class PackingListItemsController < ApplicationController
   def create
     packing_list = PackingList.find(params[:packing_list_id])
 
-    if params[:name].present? && params[:category].present?
-      new_item = Item.find_or_create_by(name: params[:name].titleize.strip, category: params[:category])
+    name = params[:name].to_s.strip
+    category = params[:category].to_s.strip
+
+    if name.present? && category.present?
+      new_item = Item.find_or_create_by(name: name.titleize, category: :category)
       packing_list.items << new_item unless packing_list.items.include?(new_item)
     end
 
@@ -17,8 +20,18 @@ class PackingListItemsController < ApplicationController
       items.each do |item|
         packing_list.items << item unless packing_list.items.include?(item)
       end
-    end
 
-    redirect_to packing_list, notice: "Items added successfully."
+      redirect_to packing_list, notice: "Items added successfully."
+    else
+      redirect_to new_packing_list_packing_list_item_path(packing_list), alert: "Items not added."
+    end
+  end
+
+  def destroy
+    @packing_list_item = PackingListItem.find(params[:id])
+    packing_list = @packing_list_item.packing_list
+    @packing_list_item.destroy
+
+    redirect_to packing_list_path(packing_list), notice: "Item deleted!"
   end
 end
