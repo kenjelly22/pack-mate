8911c00f6bc62c937286e05761460084d2718977
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-12 02:13:27 -0400

    Change status when item box is checked and display message next to item to show packed

--- app/controllers/packing_lists_controller.rb
+++ app/controllers/packing_lists_controller.rb
@@ -24,7 +24,25 @@ class PackingListsController < ApplicationController
       redirect_to packing_lists_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
 
+  def update
+    @packing_list = current_user.packing_lists.find(params[:id])
+
+    if params[:packed_item_ids].present?
+      @packing_list.packing_list_items.each do |pli|
+        new_status = params[:packed_item_ids].include?(pli.id.to_s) ? "packed" : "not_packed"
+        pli.update(status: new_status)
+      end
+    else
+      @packing_list.packing_list_items.update_all(status: "not_packed")
+    end
+
+    if @packing_list.update(packing_list_params)
+      redirect_to @packing_list, notice: "Packing list updated"
+    else
+      render :show, status: :unprocessable_entity
     end
   end
 
