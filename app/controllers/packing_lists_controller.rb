4c4e5a994bc5978876a00c8ca6f48c695d702aa3
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-12 02:02:54 -0400

    Organize items under category headings on packing list page

--- app/controllers/packing_lists_controller.rb
+++ app/controllers/packing_lists_controller.rb
@@ -7,7 +7,7 @@ class PackingListsController < ApplicationController
 
   def show
     @packing_list = current_user.packing_lists.find(params[:id])
-    @packing_list_items = @packing_list.items
+    @packing_list_items = @packing_list.packing_list_items.includes(:item)
     ##
     @items = Item.all_for_user(current_user)
     ##
